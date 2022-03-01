aba3a74e6590d2e50e931500f0839fc4d5780218
BigBen83 <benjamin.toubhans@orange.fr>
2022-03-01 18:54:54 +0100

    Co-authored-by: yves40 <yves40@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,29 +1,45 @@
-console.log ("coucou");
 
-const slide = ["/images/image1.jpg", "/images/image2.jpg", "/images/image3.jpg"];
+const slider = {
+     slide: ["/images/image1.jpg", "/images/image2.jpg", "/images/image3.jpg"],
+     desc: ["tome1", "tome2", "tome3"],
+     comment: ["description du tome1", "description du tome2", "description du tome3" ]
+}
+
 let numero = 0;
+let isPaused = false
+let boutonPause = document.getElementById("pause_button")
+let p = document.getElementById("description")
+p.innerHTML = slider.desc[0];
 
 function changeSlide(sens) {
     numero = numero + sens;
     if (numero < 0) {
-        numero = slide.length -1;
+        numero = slider.slide.length -1;
     }
-    if (numero > slide.length -1) {
+    if (numero > slider.slide.length -1) {
         numero = 0;
     }
-    document.getElementById("slide").src = slide[numero];
+    document.getElementById("slide").src = slider.slide[numero];
+    document.getElementById("description").innerHTML = slider.desc[numero];
 }
 
-let isPaused = false
 
 document.getElementById("pause_button").addEventListener('click', togglePause)
 document.getElementById("previous").addEventListener('click', () => changeSlide(-1))
 document.getElementById("next").addEventListener('click', () => changeSlide(1))
 
-/*bouton play : <i class="fa-regular fa-circle-play"></i>*/
+/*bouton play :  class="fa-circle-play"  */
+/*bouton pause : class="fa-circle-pause" */
+
 
 function togglePause() {
     isPaused = !isPaused
+    if (isPaused === true){
+        boutonPause.classList.replace("fa-circle-pause", "fa-circle-play")
+    }
+    else {
+        boutonPause.classList.replace("fa-circle-play", "fa-circle-pause")
+    }
 }
 
 function autoDefil() {
