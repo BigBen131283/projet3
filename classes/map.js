d0a32f38a0684b2a87192e78e2132bc6969d973f
yves40 <yves.toubhans@free.fr>
2022-03-08 11:06:10 +0100

    Problem to access this in event handler

--- classes/map.js
+++ classes/map.js
@@ -6,30 +6,40 @@
     Mar 03 2022   Play with markers. 
     Mar 04 2022   Test MAPQUEST. 
     Mar 05 2022   Test MAPQUEST. 
+    Mar 08 2022   Test MAPQUEST. Play with controls, zoom, etc...
 
 */
 
 export default class map {
 
+  #defaultzoom = 12;
+
   constructor () {
-    this.version = "map.js 1.25 Mar 06 2022 : "
+    this.version = "map.js 1.32 Mar 08 2022 : "
     this.map = null;
     this.mapquestkey = 'rQpw7O2I6ADzhQAAJLS4vZZ5PN7TLMX2';
     this.cityname = null;
+    this.currentzoom = this.#defaultzoom;
   }
-  // ----------------------------------------------- Open Layer map
+  // ----------------------------------------------- Mapquest / leaflet map
   createMap(selectedcity) {
     this.cityname = selectedcity.name;
     L.mapquest.key = this.mapquestkey;
     this.map = L.mapquest.map('map', {
       center: selectedcity.coord,
       layers: L.mapquest.tileLayer('map'),
-      zoom: 12,   // Zoom range from 1 to 18, the greater the more focused
-      minZoom: 10,
-      maxZoom: 16,
-      zoomlevelschange: this.zoomLevelChange,
+      zoom: this.currentzoom,     
+                    // Zoom range from 1 to 18, the greater the more focused
+      minZoom: 10,  // No need to dezoom on the entire world
+      maxZoom: 16,  // No need to zoom on a single street
     });
-    this.map.on('click', this.click);
+    L.control.layers({
+      'Map': L.mapquest.tileLayer('map'),
+      'Satellite': L.mapquest.tileLayer('satellite'),
+    }).addTo(this.map);
+    L.control.scale({"imperial": false}).addTo(this.map);
+    this.map.on('click', this.click);               // Take some action on click
+    this.map.on('zoomanim',this.zoomLevelChange, this);   // Handle zoom change
     this.log('Map created for ' + selectedcity.name + ' on ' + selectedcity.coord);
     return this;
   }
@@ -43,14 +53,19 @@ export default class map {
     this.map.setView(selectedcity.coord, this.zoom);
     this.log(`Now displaying ${this.cityname} stations (${nstations})`);
   }
+
   // ----------------------------------------------- 
-  click(ev) {
-    console.log(ev.latlng); // ev is an event object (MouseEvent in this case)
+  //  Some map event handlers
+  // ----------------------------------------------- 
+  click(clickevent) {
+    console.log(clickevent.latlng); // clickevent is a MouseEvent object
   };
   // ----------------------------------------------- 
-  zoomLevelChange(event) {
-    console.log('Zoom level changed to : ' + event)
+  zoomLevelChange(zoomevent) {
+    console.log( zoomevent.zoom);
   }
+   // ----------------------------------------------- 
+  //  Misc
   // ----------------------------------------------- 
   getVersion() {
     return this.version;
